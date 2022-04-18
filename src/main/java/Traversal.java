0b3d31e4a25e9f584796f373073412664b7cd8d3
jphuang <jphuang0@gmail.com>
2022-04-18 12:02:42 +0800

    [104]二叉树的最大深度

--- src/main/java/Traversal.java
+++ src/main/java/Traversal.java
@@ -4,33 +4,34 @@ import java.util.Deque;
 import java.util.List;
 
 public class Traversal {
-    public static List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        inorder(root,res);
-        return  res;
+  public static List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    inorder(root, res);
+    return res;
+  }
+
+  public static void inorder(TreeNode root, List<Integer> res) {
+    if (root == null) {
+      return;
+    }
+    inorder(root.left, res);
+    res.add(root.val);
+    inorder(root.right, res);
+  }
+
+  public static List<Integer> inorderTraversalByDeque(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    while (root != null || !stack.isEmpty()) {
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      res.add(root.val);
+      root = root.right;
     }
 
-    public static  void inorder(TreeNode root,List<Integer> res) {
-        if (root == null) {
-            return;
-        }
-        inorder(root.left,res);
-        res.add(root.val);
-        inorder(root.right,res);
-    }
-
-    public static List<Integer> inorderTraversalByDeque(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-         while (root !=  null ||  !stack.isEmpty()) {
-             while(root != null) {
-                 stack.push(root);
-                 root = root.left ;
-             }
-             root = stack.pop();
-             res.add(root.val);
-             root = root.right;
-         }
-        return  res;
-    }
+    return res;
+  }
 }
