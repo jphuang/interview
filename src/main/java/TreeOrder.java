328a975a058d7487d95b4b8b13b2e15b6d4bc8ec
jphuang <jphuang0@gmail.com>
2022-04-18 12:34:52 +0800

    [102]二叉树的层次遍历-不用递归

--- src/main/java/TreeOrder.java
+++ src/main/java/TreeOrder.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TreeOrder {
   public List<List<Integer>> levelOrder(TreeNode root) {
@@ -22,5 +24,32 @@ public class TreeOrder {
     if (root.right != null) {
       order(root.right, level + 1, levels);
     }
+  }
+
+  public List<List<Integer>> levelOrder2(TreeNode root) {
+    List<List<Integer>> levels = new ArrayList<>();
+    if (root == null) {
+      return levels;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      List<Integer> level = new ArrayList<>();
+      int size = queue.size();
+      // size不能写在fori里面
+      for (int i = 0; i < size; i++) {
+        TreeNode poll = queue.poll();
+        level.add(poll.val);
+        if (poll.left != null) {
+          queue.offer(poll.left);
+        }
+        if (poll.right != null) {
+          queue.offer(poll.right);
+        }
+      }
+      levels.add(level);
+    }
+
+    return levels;
   }
 }
