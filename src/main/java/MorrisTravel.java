be2169df4ae479cb3c33c899aae5d989515107de
jphuang <jphuang0@gmail.com>
2022-04-18 11:17:28 +0800

    二叉树的莫里斯遍历

---
+++ src/main/java/MorrisTravel.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MorrisTravel {
+    public static TreeNode getPredecessor(TreeNode root) {
+        TreeNode pre = root ;
+        if (root.left != null) {
+            pre = root.left ;
+            while (pre.right != null && pre.right != root) {
+                pre = pre.right ;
+            }
+        }
+        return pre ;
+    }
+
+
+    public static List<Integer> morrisTravel(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        while (root != null ){
+            if (root.left == null) {
+                res.add(root.val);
+                root = root.right ;
+            }else {
+                TreeNode pre = getPredecessor(root) ;
+                if (pre.right == null ){
+                    pre.right = root ;
+                    root = root.left ;
+                }
+                if (pre.right == root){
+                    pre.right = null ;
+                    res.add(root.val);
+                    root = root.right ;
+                }
+            }
+        }
+        return res ;
+    }
+}
--- src/main/java/Traversal.java
+++ src/main/java/Traversal.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Traversal {
@@ -15,5 +17,20 @@ public class Traversal {
         inorder(root.left,res);
         res.add(root.val);
         inorder(root.right,res);
+    }
+
+    public static List<Integer> inorderTraversalByDeque(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+         while (root !=  null ||  !stack.isEmpty()) {
+             while(root != null) {
+                 stack.push(root);
+                 root = root.left ;
+             }
+             root = stack.pop();
+             res.add(root.val);
+             root = root.right;
+         }
+        return  res;
     }
 }
