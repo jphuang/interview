3150da54c25b6cf72f01baaa9e748840041dbc2b
jphuang <jphuang0@gmail.com>
2022-04-19 10:58:02 +0800

    101. 对称二叉树-递归

--- src/main/java/TreeSame.java
+++ src/main/java/TreeSame.java
@@ -11,4 +11,21 @@ public class TreeSame {
     }
     return false;
   }
+  // 101. 对称二叉树
+  public static boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    return symmetric(root.left, root.right);
+  }
+
+  public static boolean symmetric(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    return p.val == q.val && symmetric(p.left, q.right) && symmetric(p.right, q.left);
+  }
 }
