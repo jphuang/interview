834ffc7d4966e6078e9c610d552d1b4cdda84690
jphuang <jphuang0@gmail.com>
2022-04-19 11:47:38 +0800

    101. 对称二叉树-遍历

--- src/main/java/TreeSame.java
+++ src/main/java/TreeSame.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeSame {
   public static boolean isSameTree(TreeNode p, TreeNode q) {
     if (p == null && q == null) {
@@ -27,5 +30,42 @@ public class TreeSame {
       return false;
     }
     return p.val == q.val && symmetric(p.left, q.right) && symmetric(p.right, q.left);
+  }
+
+  // 101. 对称二叉树- 遍历
+  public static boolean isSymmetric2(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    if (root.left == null && root.right == null) {
+      return true;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    Queue<TreeNode> p = new LinkedList<>();
+
+    p.offer(root.left);
+    q.offer(root.right);
+
+    while (!q.isEmpty() || !p.isEmpty()) {
+
+      TreeNode first = p.poll();
+      TreeNode last = q.poll();
+      if (first == null && last == null) {
+        continue;
+      }
+      if (first == null || last == null) {
+        return false;
+      }
+
+      if (first.val != last.val) {
+        return false;
+      }
+      p.offer(first.left);
+      p.offer(first.right);
+
+      q.offer(last.right);
+      q.offer(last.left);
+    }
+    return true;
   }
 }
--- src/main/test/TreeSameTest.java
+++ src/main/test/TreeSameTest.java
@@ -8,4 +8,24 @@ public class TreeSameTest {
     boolean sameTree = TreeSame.isSameTree(root, root);
     assert sameTree;
   }
+
+  @Test
+  public void TestSymmetric2() {
+    TreeNode root =
+        new TreeNode(
+            1, new TreeNode(2, new TreeNode(3), null), new TreeNode(2, null, new TreeNode(3)));
+    boolean sameTree = TreeSame.isSymmetric2(root);
+    assert !sameTree;
+  }
+
+  @Test
+  public void TestSymmetric3() {
+    TreeNode root =
+        new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+    boolean sameTree = TreeSame.isSymmetric2(root);
+    assert sameTree;
+  }
 }
