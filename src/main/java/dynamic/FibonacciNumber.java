2697e0216534ea758b16e79b7467f467b3f4b2d3
jphuang <jphuang0@gmail.com>
2022-04-22 12:08:23 +0800

    1137. 第 N 个泰波那契数

--- src/main/java/dynamic/FibonacciNumber.java
+++ src/main/java/dynamic/FibonacciNumber.java
@@ -50,4 +50,21 @@ public class FibonacciNumber {
     f[n] = fib3Helper(n - 1, f) + fib3Helper(n - 2, f);
     return f[n];
   }
+
+  public int tribonacci(int n) {
+    if (n <= 0) {
+      return 0;
+    }
+    if (n == 1) {
+      return 1;
+    }
+    int[] f = new int[n + 1];
+    f[0] = 0;
+    f[1] = 1;
+    f[2] = 1;
+    for (int i = 3; i <= n; i++) {
+      f[i] = f[i - 1] + f[i - 2] + f[i - 3];
+    }
+    return f[n];
+  }
 }
