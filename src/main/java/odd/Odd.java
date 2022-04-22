fc54f2653ef21822a05fab3b43a13071142dbb9c
jphuang <jphuang0@gmail.com>
2022-04-22 12:34:23 +0800

    1491. 去掉最低工资和最高工资后的工资平均值

--- src/main/java/odd/Odd.java
+++ src/main/java/odd/Odd.java
@@ -12,4 +12,23 @@ public class Odd {
     }
     return (high - low) >> 1;
   }
+  // 1491. 去掉最低工资和最高工资后的工资平均值
+  // 给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。
+  //
+  // 请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。
+  public double average(int[] salary) {
+    int max = salary[0];
+    int min = salary[0];
+    double sum = salary[0];
+    for (int i = 1; i < salary.length; i++) {
+      if (salary[i] < min) {
+        min = salary[i];
+      }
+      if (salary[i] > max) {
+        max = salary[i];
+      }
+      sum += salary[i];
+    }
+    return (sum - min - max) / (salary.length - 2);
+  }
 }
