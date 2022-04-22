5c84a992f12c3982a468d731cde318aa482dbdcf
jphuang <jphuang0@gmail.com>
2022-04-22 12:25:12 +0800

    1523. 在区间范围内统计奇数数目

---
+++ src/main/java/odd/Odd.java
@@ -0,0 +1,15 @@
+package odd;
+
+public class Odd {
+  // 1523. 在区间范围内统计奇数数目
+  // 给你两个非负整数 low 和 high 。请你返回 low 和 high 之间（包括二者）奇数的数目。
+  public int countOdds(int low, int high) {
+    if (low % 2 == 1) {
+      low--;
+    }
+    if (high % 2 == 1) {
+      high++;
+    }
+    return (high - low) >> 1;
+  }
+}
