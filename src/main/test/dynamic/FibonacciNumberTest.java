8ee6aa9091174300936bdeb94ad2eff36566e817
jphuang <jphuang0@gmail.com>
2022-04-22 11:54:31 +0800

    509. 斐波那契数

---
+++ src/main/test/dynamic/FibonacciNumberTest.java
@@ -0,0 +1,23 @@
+package dynamic;
+
+import org.junit.jupiter.api.Test;
+
+public class FibonacciNumberTest {
+  @Test
+  public void testFib() {
+    int fib = FibonacciNumber.fib(2);
+    assert fib == 1;
+
+    fib = FibonacciNumber.fib(0);
+    assert fib == 0;
+  }
+
+  @Test
+  public void testFib2() {
+    int fib = FibonacciNumber.fib2(2);
+    assert fib == 1;
+
+    fib = FibonacciNumber.fib2(0);
+    assert fib == 0;
+  }
+}
