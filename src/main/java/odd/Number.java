8bcc9eb801d16f1349c2f1ddd599d0a2941ebd34
jphuang <jphuang0@gmail.com>
2022-04-23 11:28:00 +0800

    1281. 整数的各位积和之差

--- src/main/java/odd/Number.java
+++ src/main/java/odd/Number.java
@@ -19,4 +19,17 @@ public class Number {
     }
     return ret;
   }
+
+  // 1281. 整数的各位积和之差
+  // 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+  public int subtractProductAndSum(int n) {
+    int sum = 0;
+    int mul = 1;
+    while (n != 0) {
+      sum += n % 10;
+      mul *= n % 10;
+      n = n / 10;
+    }
+    return mul - sum;
+  }
 }
