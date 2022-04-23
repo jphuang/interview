1d3cebd0c8ac087c490d0ad52c58cc5d2f4b5262
jphuang <jphuang0@gmail.com>
2022-04-23 11:55:23 +0800

    746. 使用最小花费爬楼梯

--- src/main/java/dynamic/ClimbStairs.java
+++ src/main/java/dynamic/ClimbStairs.java
@@ -23,4 +23,27 @@ public class ClimbStairs {
     }
     return f;
   }
+  // 746. 使用最小花费爬楼梯
+  // 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+  public int minCostClimbingStairs(int[] cost) {
+    int n = cost.length;
+    int[] dp = new int[n + 1];
+    dp[0] = dp[1] = 0;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    return dp[n];
+  }
+
+  public int minCostClimbingStairs2(int[] cost) {
+    int n = cost.length;
+    int curr = 0;
+    int pre = 0;
+    for (int i = 2; i <= n; i++) {
+      int next = Math.min(pre + cost[i - 1], curr + cost[i - 2]);
+      pre = curr;
+      curr = next;
+    }
+    return curr;
+  }
 }
