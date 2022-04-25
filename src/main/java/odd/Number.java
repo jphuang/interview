688c6b15bba823d770122b6c3bd388ae63f71fec
jphuang <jphuang0@gmail.com>
2022-04-25 11:07:17 +0800

    1502. 判断能否形成等差数列

--- src/main/java/odd/Number.java
+++ src/main/java/odd/Number.java
@@ -1,5 +1,7 @@
 package odd;
 
+import java.util.Arrays;
+
 public class Number {
   public static int hammingWeight(int n) {
     int ret = 0;
@@ -53,5 +55,30 @@ public class Number {
       }
     }
     return ret;
+  }
+
+  // 1502. 判断能否形成等差数列
+  // 给你一个数字数组 arr 。
+  //
+  // 如果一个数列中，任意相邻两项的差总等于同一个常数，那么这个数列就称为 等差数列 。
+  //
+  // 如果可以重新排列数组形成等差数列，请返回 true ；否则，返回 false 。
+  public boolean canMakeArithmeticProgression(int[] arr) {
+    if (arr == null || arr.length == 0) {
+      return false;
+    }
+    int length = arr.length;
+    if (length == 1 || length == 2) {
+      return true;
+    }
+    Arrays.sort(arr);
+    int n = arr[0] - arr[1];
+    for (int i = 1; i < length - 1; i++) {
+      if ((arr[i] - arr[i + 1]) != n) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
