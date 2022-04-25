5dcb1d34ded15e3400c400b74549e556ac634692
jphuang <jphuang0@gmail.com>
2022-04-25 16:20:53 +0800

    45. 跳跃游戏 II

--- src/main/java/dynamic/Jump.java
+++ src/main/java/dynamic/Jump.java
@@ -27,10 +27,17 @@ public class Jump {
     }
     return false;
   }
-
-  public boolean canJump2(int[] nums) {
+  // 45. 跳跃游戏 II
+  // 给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+  //
+  // 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+  //
+  // 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+  //
+  // 假设你总是可以到达数组的最后一个位置
+  public int jump(int[] nums) {
     if (nums == null || nums.length == 0) {
-      return false;
+      return 0;
     }
     int position = nums.length - 1;
     int steps = 0;
@@ -43,6 +50,6 @@ public class Jump {
         }
       }
     }
-    return true;
+    return steps;
   }
 }
--- src/main/test/dynamic/JumpTest.java
+++ src/main/test/dynamic/JumpTest.java
@@ -9,4 +9,11 @@ public class JumpTest {
     boolean b = jump.canJump(new int[] {2, 3, 1, 1, 4});
     assert b;
   }
+
+  @Test
+  public void testJump() {
+    Jump jump = new Jump();
+    int b = jump.jump(new int[] {2, 3, 1, 1, 4});
+    assert b == 2;
+  }
 }
