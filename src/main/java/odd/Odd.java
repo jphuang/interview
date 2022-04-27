b5281c1deeae90549c25e741ce13dd0124bdfd57
jphuang <jphuang0@gmail.com>
2022-04-27 12:26:44 +0800

    1672. 最富有客户的资产总量

--- src/main/java/odd/Odd.java
+++ src/main/java/odd/Odd.java
@@ -67,4 +67,20 @@ public class Odd {
       nums[i] = 0;
     }
   }
+  // 1672. 最富有客户的资产总量
+  // 给你一个 m x n 的整数网格 accounts ，其中 accounts[i][j] 是第 i​​​​​​​​​​​​ 位客户在第 j 家银行托管的资产数量。返回最富有客户所拥有的
+  // 资产总量 。
+  //
+  // 客户的 资产总量 就是他们在各家银行托管的资产数量之和。最富有客户就是 资产总量 最大的客户
+  public int maximumWealth(int[][] accounts) {
+    int max = 0;
+    for (int[] account : accounts) {
+      int sum = 0;
+      for (int i : account) {
+        sum += i;
+      }
+      max = Math.max(sum, max);
+    }
+    return max;
+  }
 }
