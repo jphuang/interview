2a038ddbc7222569a6462f408c337c71fd0ed57b
jphuang <jphuang0@gmail.com>
2022-05-01 13:13:18 +0800

    714. 买卖股票的最佳时机含手续费

--- src/main/java/dynamic/Profit.java
+++ src/main/java/dynamic/Profit.java
@@ -63,4 +63,22 @@ public class Profit {
     }
     return max;
   }
+  // 714. 买卖股票的最佳时机含手续费
+  // 给定一个整数数组 prices，其中 prices[i]表示第 i 天的股票价格 ；整数 fee 代表了交易股票的手续费用。
+  //
+  // 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+  //
+  // 返回获得利润的最大值。
+  //
+  // 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+  public int maxProfitWithFee(int[] prices, int fee) {
+    int max = 0;
+    int dp_i_1 = Integer.MIN_VALUE;
+    for (int i = 0; i < prices.length; i++) {
+      int temp = max;
+      max = Math.max(max, dp_i_1 + prices[i]);
+      dp_i_1 = Math.max(dp_i_1, temp - prices[i] - fee);
+    }
+    return max;
+  }
 }
