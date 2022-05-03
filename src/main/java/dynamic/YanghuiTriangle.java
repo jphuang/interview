326f68622faa62ccc129489b38327663cf72b4ff
jphuang <jphuang0@gmail.com>
2022-05-03 13:16:06 +0800

    119. 杨辉三角 II

--- src/main/java/dynamic/YanghuiTriangle.java
+++ src/main/java/dynamic/YanghuiTriangle.java
@@ -25,4 +25,37 @@ public class YanghuiTriangle {
     }
     return ans;
   }
+  // 119. 杨辉三角 II
+  // 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+  //
+  // 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+  public List<Integer> getRow(int rowIndex) {
+    List<Integer> pre = new LinkedList<>();
+    for (int i = 0; i <= rowIndex; i++) {
+      List<Integer> list = new LinkedList<>();
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          list.add(1);
+          continue;
+        }
+        // 上一行的两个数
+        list.add(pre.get(j - 1) + pre.get(j));
+      }
+      pre = list;
+    }
+    return pre;
+  }
+
+  public List<Integer> getRow2(int rowIndex) {
+    List<Integer> pre = new LinkedList<>();
+    pre.add(1);
+    for (int i = 1; i <= rowIndex; i++) {
+      for (int j = i; j > 0; j--) {
+        // 上一行的两个数
+        pre.add(0);
+        pre.set(j, pre.get(j - 1) + pre.get(j));
+      }
+    }
+    return pre;
+  }
 }
