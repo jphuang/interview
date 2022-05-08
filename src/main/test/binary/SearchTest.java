e76b899c12cea0dc7852873cea56a0c2b77c1130
jphuang <jphuang0@gmail.com>
2022-05-08 15:04:31 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- src/main/test/binary/SearchTest.java
+++ src/main/test/binary/SearchTest.java
@@ -18,4 +18,28 @@ public class SearchTest {
     System.out.println(index);
     assert index == 0;
   }
+
+  @Test
+  public void testSearchRange() {
+    Search search = new Search();
+    int[] index = search.searchRange(new int[] {5, 7, 7, 8, 8, 10}, 8);
+    assert index[0] == 3;
+    assert index[1] == 4;
+  }
+
+  @Test
+  public void testSearchRange2() {
+    Search search = new Search();
+    int[] index = search.searchRange(new int[] {5, 7, 7, 8, 8, 10}, 6);
+    assert index[0] == -1;
+    assert index[1] == -1;
+  }
+
+  @Test
+  public void testSearchRange3() {
+    Search search = new Search();
+    int[] index = search.searchRange(new int[] {1}, 0);
+    assert index[0] == -1;
+    assert index[1] == -1;
+  }
 }
