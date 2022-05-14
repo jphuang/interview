6dbc3abe176bf29caf84ec17eeae98d10ed9f30a
jphuang <jphuang0@gmail.com>
2022-05-14 10:18:44 +0800

    1337. 矩阵中战斗力最弱的 K 行

--- src/main/java/binary/Matrix.java
+++ src/main/java/binary/Matrix.java
@@ -1,5 +1,10 @@
 package binary;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Matrix {
 
   /**
@@ -57,5 +62,53 @@ public class Matrix {
       }
     }
     return false;
+  }
+  // 1337. 矩阵中战斗力最弱的 K 行
+  // 给你一个大小为 m * n 的矩阵 mat，矩阵由若干军人和平民组成，分别用 1 和 0 表示。
+  // 请你返回矩阵中战斗力最弱的 k 行的索引，按从最弱到最强排序。
+  // 如果第 i 行的军人数量少于第 j 行，或者两行军人数量相同但 i 小于 j，那么我们认为第 i 行的战斗力比第 j 行弱。
+  // 军人 总是 排在一行中的靠前位置，也就是说 1 总是出现在 0 之前。。
+  public int[] kWeakestRows(int[][] mat, int k) {
+    int m = mat.length;
+    int n = mat[0].length;
+    List<int[]> power = new ArrayList<int[]>();
+    // 二分查找第一个0的位置
+    for (int i = 0; i < m; i++) {
+      int left = 0;
+      int right = n - 1;
+      int pos = -1;
+      while (left <= right) {
+        int mid = (left + right) / 2;
+        if (mat[i][mid] == 0) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+          pos = mid;
+        }
+      }
+      power.add(new int[] {pos + 1, i});
+    }
+
+    // 最小堆
+    PriorityQueue<int[]> queue =
+        new PriorityQueue<>(
+            new Comparator<int[]>() {
+              @Override
+              public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                  return o1[0] - o2[0];
+                } else {
+                  return o1[1] - o2[1];
+                }
+              }
+            });
+    for (int[] ints : power) {
+      queue.offer(ints);
+    }
+    int[] ans = new int[k];
+    for (int i = 0; i < k; i++) {
+      ans[i] = queue.poll()[1];
+    }
+    return ans;
   }
 }
