89864ddc61eed2a061ead53028f45a7f0853aadb
jphuang <jphuang0@gmail.com>
2022-08-17 12:50:08 +0800

    392. 判断子序列

--- src/main/test/character/IsomorphicTest.java
+++ src/main/test/character/IsomorphicTest.java
@@ -1,5 +1,6 @@
 package character;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class IsomorphicTest {
@@ -15,5 +16,18 @@ public class IsomorphicTest {
         assert isomorphic;
         isomorphic = test.isIsomorphic("badc", "baba");
         assert !isomorphic;
+    }
+
+    @Test
+    public void isSubsequence() {
+        Isomorphic test = new Isomorphic();
+        boolean subsequence = test.isSubsequence("abc", "ahbgdc");
+        Assert.assertTrue(subsequence);
+        subsequence = test.isSubsequence("axc", "ahbgdc");
+        Assert.assertFalse(subsequence);
+        subsequence = test.isSubsequence("aaaaaa", "bbaaaa");
+        Assert.assertFalse(subsequence);
+        subsequence = test.isSubsequence("bb", "ahbgdc");
+        Assert.assertFalse(subsequence);
     }
 }
